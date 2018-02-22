f0ed7603a00e9f2c9d2e2eb587aad285e80f23d5
ruden <ruden91@mycelebs.com>
2018-02-22 12:31:22 +0900

    react 초기세팅 및 default css, web font 추가

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// webFont loader
+
+// default css setting
+import 'normalize.css';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
